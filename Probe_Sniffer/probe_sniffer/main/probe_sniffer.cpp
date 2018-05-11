05f2005ee5a2e2a65add603abd9ac9ddbb93f323
AlbertoRiorda95 <30572128+AlbertoRiorda95@users.noreply.github.com>
2018-05-11 08:50:00 +0200

    Add files via upload

--- Probe_Sniffer/probe_sniffer/main/probe_sniffer.cpp
+++ Probe_Sniffer/probe_sniffer/main/probe_sniffer.cpp
@@ -10,8 +10,6 @@ using namespace std;
 /* Includes */
 #include <memory>
 #include <vector>
-#include "freertos/FreeRTOS.h"
-#include "freertos/event_groups.h"
 #include "esp_wifi.h"
 #include "esp_log.h"
 #include "esp_event_loop.h"
@@ -20,10 +18,7 @@ using namespace std;
 #include "lwip/sys.h"
 #include "lwip/api.h"
 #include "lwip/sockets.h"
-#include "lwip/netbuf.h"
-#include "lwip/netdb.h"
-#include "lwip/netifapi.h"
-#include "lwip/pppapi.h"
+#include "driver/timer.h"
 #include "Packet.h"
 
 /* Defines */
@@ -47,7 +42,7 @@ struct wifi_packet_t {
 struct ssid_t {
 	uint8_t id;
 	uint8_t lenght;
-	uint8_t *ssid;
+	uint8_t ssid[0];
 };
 
 /* Enums */
@@ -58,39 +53,42 @@ enum modes {
 };
 
 /* Functions prototypes */
-
 void probe_sniffer(void);
 /* Initializations */
 static void wifi_init(void);
 static void socket_server_init(void);
 static void socket_client_init(void);
-static void timer_init(void);
-/* Mode change */
-static void set_to_sniffer(void);
-static void set_to_client(void);
-static void set_to_server(void);
+static void sniffer_timer_init(void);
 /* Handlers */
 static esp_err_t event_handler(void *ctx, system_event_t *event);
-static void promiscuousCb(void *buffer, wifi_promiscuous_pkt_type_t type);
+static void sniffer_callback(void *buffer, wifi_promiscuous_pkt_type_t type);
+static void timer_callback(void *arg);
 /* Socket communication */
 static void socket_send_data(void);
 static void socket_receive_data(void);
+/* Sniffer */
+static void sniffer_on(void);
+static void sniffer_off(void);
 /* Utilities */
 static void printMac(uint8_t mac[6]);
 
 /* Global variables */
 static const char *TAG = "probe_sniffer";
 static vector<Packet> packets_list;
-static wifi_config_t wifi_config;
-static int s_fd, i = 0;
+static int s_fd, i = 0, conn_counter = 0;
 static enum modes mode = SERVER;
 struct sockaddr_in caddr;
+esp_err_t err;
+
+/* Main C++ declaration */
+extern "C"
+{
+	void app_main();
+}
 
 /* Main */
-extern "C" {
-void app_main();
-}
-void app_main(){
+void app_main()
+{
 	probe_sniffer();
 }
 
@@ -114,29 +112,20 @@ static void wifi_init(void)
 
     ESP_ERROR_CHECK(esp_event_loop_init(event_handler, NULL));
 
+    /* Define configurations */
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-
-    /* Define the AP */
+	ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+	wifi_config_t wifi_config;
     memset(&wifi_config, 0, sizeof(wifi_config));
-    strcpy((char*)wifi_config.sta.ssid, "AndroidAP");
-    strcpy((char*)wifi_config.sta.password, "fprl1297");
+    strcpy((char*)wifi_config.sta.ssid, CONFIG_WIFI_SSID);
+    strcpy((char*)wifi_config.sta.password, CONFIG_WIFI_PASSWORD);
+
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+	ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
+	ESP_ERROR_CHECK(esp_wifi_start());
 
     printf("\nSystem ready.\n\n");
 
-    /* First set to server mode and wait */
-    set_to_server();
-
-    /* Example */
-    /*ESP_ERROR_CHECK(esp_wifi_set_promiscuous_rx_cb(&promiscuousCb));
-	ESP_ERROR_CHECK(esp_wifi_set_promiscuous(true));
-	ESP_ERROR_CHECK(esp_wifi_set_channel(1, WIFI_SECOND_CHAN_ABOVE));
-
-	ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
-	ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
-
-	is_sniffer = true;
-	ESP_ERROR_CHECK(esp_wifi_start());*/
 }
 
 /* Initialize server socket */
@@ -155,56 +144,38 @@ static void socket_server_init(void)
 static void socket_client_init(void)
 {
 	s_fd = socket(AF_INET, SOCK_STREAM, 0);
+	connect(s_fd, (sockaddr *) &caddr, sizeof(caddr));
 }
 
-static void timer_init(void)
+/* Initialize timer */
+static void sniffer_timer_init(void)
 {
-
+	timer_config_t config = {
+			.alarm_en = true,
+			.counter_en = true,
+			.intr_type = TIMER_INTR_LEVEL,
+			.counter_dir = TIMER_COUNT_DOWN,
+			.auto_reload = false,
+			.divider = 16
+	};
+	ESP_ERROR_CHECK(timer_init(TIMER_GROUP_0, TIMER_0, &config));
+	ESP_ERROR_CHECK(timer_set_counter_value(TIMER_GROUP_0, TIMER_0, 60000));
+	ESP_ERROR_CHECK(timer_set_alarm_value(TIMER_GROUP_0, TIMER_0, 0));
+	ESP_ERROR_CHECK(timer_isr_register(TIMER_GROUP_0, TIMER_0, timer_callback, (void *) NULL, ESP_INTR_FLAG_IRAM, NULL));
 }
 
-/* Sniffer mode activated */
-static void set_to_sniffer(void)
+/* Handler for timer time-out */
+static void timer_callback(void *arg)
 {
-	//ESP_ERROR_CHECK(esp_wifi_stop());
-	packets_list.clear();
-
-	ESP_ERROR_CHECK(esp_wifi_set_promiscuous_rx_cb(&promiscuousCb));
-	//ESP_ERROR_CHECK(esp_wifi_set_channel(1, WIFI_SECOND_CHAN_ABOVE));
-	ESP_ERROR_CHECK(esp_wifi_set_promiscuous(true));
-
-	ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
-	ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
-
-	mode = SNIFFER;
-	//ESP_ERROR_CHECK(esp_wifi_start());
+	TIMERG0.int_clr_timers.t0 = 1;
+	sniffer_off();
+	socket_send_data();
+	sniffer_timer_init();
+	sniffer_on();
 }
 
-/* Server mode activated, called once during initialization */
-static void set_to_server(void)
-{
-	ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
-	ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
-
-	socket_server_init();
-	mode = SERVER;
-	ESP_ERROR_CHECK(esp_wifi_start());
-}
-
-/* Client mode activated */
-static void set_to_client(void)
-{
-	ESP_ERROR_CHECK(esp_wifi_stop());
-
-	ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
-	ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
-
-	socket_client_init();
-	mode = CLIENT;
-	ESP_ERROR_CHECK(esp_wifi_start());
-}
-
-/* Handler for sniffer mode */
-static void promiscuousCb(void *buffer, wifi_promiscuous_pkt_type_t type)
+/* Handler for sniffer */
+static void sniffer_callback(void *buffer, wifi_promiscuous_pkt_type_t type)
 {
 	wifi_pkt_rx_ctrl_t ctrl;
 	struct wifi_packet_t *packet;
@@ -244,9 +215,19 @@ static esp_err_t event_handler(void *ctx, system_event_t *event)
     switch (event->event_id) {
         case SYSTEM_EVENT_STA_START:
             ESP_LOGI(TAG, "SYSTEM_EVENT_STA_START");
-            if(mode != SNIFFER)
-            	ESP_ERROR_CHECK(esp_wifi_connect());
+            err = esp_wifi_connect();
+            if(err == ESP_ERR_WIFI_SSID || err == ESP_ERR_WIFI_CONN)
+            {
+            	printf("Invalid SSID or Password, change on make menuconfig/Probe Sniffer Configuration.\n");
+            	printf("Current ssid: %s", CONFIG_WIFI_SSID);
+            	/* LED */
+            }
             break;
+
+        case SYSTEM_EVENT_STA_STOP:
+        	ESP_ERROR_CHECK(esp_wifi_start());
+        	break;
+
         case SYSTEM_EVENT_STA_GOT_IP:
             ESP_LOGI(TAG, "SYSTEM_EVENT_STA_GOT_IP");
             ESP_LOGI(TAG, "Got IP: %s\n",
@@ -255,25 +236,42 @@ static esp_err_t event_handler(void *ctx, system_event_t *event)
 			{
 				socket_receive_data();
 			}
-			else if (mode == CLIENT)
-			{
-				socket_send_data();
-			}
-			close(s_fd);
-			timer_init();
-			set_to_sniffer();
+            socket_client_init();
+            sniffer_timer_init();
+			sniffer_on();
             break;
-        /*case SYSTEM_EVENT_STA_DISCONNECTED:
+
+        case SYSTEM_EVENT_STA_DISCONNECTED:
             ESP_LOGI(TAG, "SYSTEM_EVENT_STA_DISCONNECTED");
             ESP_ERROR_CHECK(esp_wifi_connect());
-            break;*/
+            /* reset sockets */
+            break;
+
         case SYSTEM_EVENT_STA_CONNECTED:
 			ESP_LOGI(TAG, "SYSTEM_EVENT_STA CONNECTED");
+			conn_counter = 0;
 			break;
+
         default:
             break;
     }
     return ESP_OK;
+}
+
+static void sniffer_on(void)
+{
+	packets_list.clear();
+
+	ESP_ERROR_CHECK(esp_wifi_set_promiscuous_rx_cb(&sniffer_callback));
+	ESP_ERROR_CHECK(esp_wifi_set_promiscuous(true));
+
+	mode = SNIFFER;
+}
+
+static void sniffer_off(void)
+{
+	ESP_ERROR_CHECK(esp_wifi_set_promiscuous(false));
+	mode = CLIENT;
 }
 
 /* Send data to the desktop application */
@@ -285,6 +283,8 @@ static void socket_send_data(void)
 /* Receive from desktop application */
 static void socket_receive_data(void)
 {
+	socket_server_init();
+
 	socklen_t l = sizeof(caddr);
 	char address[16];
 	char buffer[N];
@@ -297,6 +297,8 @@ static void socket_receive_data(void)
 	/* Test send back */
 	int err = sendto(s_fd, buffer, N, 0, (sockaddr *) &caddr, l);
 	printf("num byte: %d Send back: %s\n", err, buffer);
+
+	close(s_fd);
 }
 
 /* Print MAC address */
